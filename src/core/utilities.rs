8a960d379f6429f5ed2815592f46d09ebe0d6c2c
lukeduball <lukeduball09@gmail.com>
2026-01-08 19:50:55 -0600

    Added WorldVec which is just a wrapper over Vec which takes an Entry. An Entry is a struct to hold a spot in a WorldVec along with its index in that Vec. This allows for O(1) removal of game objects using swap_remove. Added WorldObject and WorldCollisionObject traits

--- src/core/utilities.rs
+++ src/core/utilities.rs
@@ -12,4 +12,100 @@ macro_rules! include_bytes_from_project_path {
     };
 }
 
+use std::ops::{Deref, DerefMut, Index};
+
 pub(crate) use include_str_from_project_path;
+
+pub struct Entry<T> {
+    data: T,
+    index: usize
+}
+
+impl<T> Deref for Entry<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.data
+    }
+}
+
+impl<T> DerefMut for Entry<T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.data
+    }
+}
+
+pub struct WorldVec<T> {
+    vector: Vec<Entry<T>>
+}
+
+impl<T> WorldVec<T> {
+    pub fn new() -> Self {
+        Self {
+            vector: Vec::new()
+        }
+    }
+
+    pub fn len(&self) -> usize {
+        self.vector.len()
+    }
+
+    pub fn iter(&self) -> std::slice::Iter<Entry<T>> {
+        self.vector.iter()
+    }
+
+    pub fn iter_mut(&mut self) -> std::slice::IterMut<Entry<T>> {
+        self.vector.iter_mut()
+    }
+
+    pub fn swap_remove(&mut self, index: usize) -> T {
+        let vec_size = self.vector.len();
+        let removed = self.vector.swap_remove(index);
+        if index != vec_size - 1 {
+            self.vector[index].index = index;
+        }
+        removed.data
+    }
+
+    pub fn push(&mut self, object: T) {
+        let index = self.vector.len();
+        self.vector.push(Entry { data: object, index });
+    }
+}
+
+impl<T> Index<usize> for WorldVec<T> {
+    type Output = T;
+
+    fn index(&self, index: usize) -> &Self::Output {
+        &self.vector[index]
+    }
+}
+
+impl <'a, T> IntoIterator for &'a WorldVec<T> {
+    type Item = &'a Entry<T>;
+
+    type IntoIter = std::slice::Iter<'a, Entry<T>>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.vector.iter()
+    }
+}
+
+impl <'a, T> IntoIterator for &'a mut WorldVec<T> {
+    type Item = &'a mut Entry<T>;
+
+    type IntoIter = std::slice::IterMut<'a, Entry<T>>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.vector.iter_mut()
+    }
+}
+
+impl<T> IntoIterator for WorldVec<T> {
+    type Item = Entry<T>;
+    type IntoIter = std::vec::IntoIter<Self::Item>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.vector.into_iter()
+    }
+}
--- src/core/world.rs
+++ src/core/world.rs
@@ -1,1 +1,20 @@
+use glam::Vec2;
+
+use crate::core::math::{Transform2d, bounding2d::Polygon2d};
+
 pub mod camera;
+
+pub trait WorldObject2d {
+    fn get_transform2d(&self) -> &Transform2d;
+
+    fn translate(&mut self, translation: Vec2);
+    fn set_translation(&mut self, translation: Vec2);
+    fn rotate(&mut self, rotation: f32);
+    fn set_rotation(&mut self, rotation: f32);
+    fn scale(&mut self, scale_factor: Vec2);
+    fn set_scale(&mut self, scale_factor: Vec2);
+}
+
+pub trait WorldCollisionObject2d {
+    fn get_collider(&self) -> &Polygon2d;
+}
